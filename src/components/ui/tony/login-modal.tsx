3b1712b5e2c28c8d5ff31d1e96157b934110b3ab
liangjiongxin <liangjiongxin@gmail.com>
2024-01-03 03:53:22 +0800

    feature: mod template

--- src/components/ui/tony/login-modal.tsx
+++ src/components/ui/tony/login-modal.tsx
@@ -31,6 +31,27 @@ export default function LoginModal({}: Props) {
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
+          <Button variant="outline" type="button">
+            <Icons.gitHub className="mr-2 h-4 w-4" />
+            Github
+          </Button>
+
+          <Button variant="outline" type="button">
+            <Icons.google className="mr-2 h-4 w-4" />
+            Google
+          </Button>
+
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-background px-2 text-muted-foreground">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
           <div className="grid items-center gap-2">
             <Label htmlFor="name" className="text-slate-500">
               Email Address
@@ -44,26 +65,12 @@ export default function LoginModal({}: Props) {
             <Input id="username" type="password" />
           </div>
 
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <span className="w-full border-t" />
-            </div>
-            <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">
-                Or continue with
-              </span>
-            </div>
-          </div>
-
-          <Button variant="outline" type="button">
-            <Icons.gitHub className="mr-2 h-4 w-4" />
-            Github
-          </Button>
-
-          <Button variant="outline" type="button">
-            <Icons.google className="mr-2 h-4 w-4" />
-            Google
-          </Button>
+          <p className="text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <span className="text-violet-500 hover:cursor-pointer hover:underline">
+              Sign up
+            </span>
+          </p>
         </div>
         <DialogFooter>
           <Button className="w-full" type="submit">
