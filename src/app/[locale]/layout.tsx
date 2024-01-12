fd71e34472ff6eb93884c1269abf1f8a5af81a75
liangjiongxin <liangjiongxin@gmail.com>
2024-01-13 04:07:48 +0800

    feature: update layout color

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -31,7 +31,10 @@ export default function LocaleLayout({ children, params: { locale } }: Props) {
   return (
     <html lang={locale}>
       <body>
-        <div className="fixed -z-10 h-screen w-full bg-gradient-to-br from-indigo-100 via-slate-100 to-teal-50"></div>
+        {/* <div className="fixed -z-10 h-screen w-full bg-gradient-to-br from-indigo-100 via-slate-100 to-teal-50"></div> */}
+        <div className="absolute -right-20 -top-20 z-0 h-60 w-80 bg-blue-600 opacity-30 blur-[80px]"></div>
+        <div className="absolute left-40 top-40 z-0 h-60 w-80 bg-blue-400 opacity-30 blur-[80px]"></div>
+        <div className="absolute right-40 top-40 z-0 h-60 w-80 bg-purple-400 opacity-30 blur-[80px]"></div>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <NavBar />
           {children}
